85b5c9306561fda0a654f1551fea0d45d9c3fd7e
abhinavmedikonda <abhinavmedikonda@hotmail.com>
2023-09-20 00:14:00 -0700

    Create ReadWrite.cs
    + Files

---
+++ Algorithms/Files/ReadWrite.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Algorithms.Files
+{
+    public class ReadWrite
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.CurrentDirectory);
+            string[] lines = File.ReadAllLines("abc.txt");//reads from current directory if path is not given
+            foreach (var item in lines)
+            {
+                var words = item.Trim().Split(' ');
+                Console.WriteLine(words[words.Length-1]);
+            }
+            File.WriteAllLines("xyz.txt", lines);
+            string text = File.ReadAllText("/Users/abhi/Projects/Algorithms/Algorithms/bin/Debug/netcoreapp3.1/abc.txt");
+            File.WriteAllText("xyz.txt", text);
+
+            Console.WriteLine($"test{Environment.NewLine}it");
+
+            Console.Read(); 
+        }
+    }
+}
