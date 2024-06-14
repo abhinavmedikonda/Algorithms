a803010bbb2a03936dd64bedec009b419994f992
abhinav medikonda <abhinavmedikonda@hotmail.com>
2024-06-13 22:54:49 -0700

    * main commented

--- Algorithms/Arrays/MaximumSumSubArray.cs
+++ Algorithms/Arrays/MaximumSumSubArray.cs
@@ -40,21 +40,21 @@ public class MaximumSumSubArray
 2 -1 2 3 4 -5
 */
 
-    public static void Main(string[] args)
-    {
-        int t = Convert.ToInt32(Console.ReadLine().Trim());
+    // public static void Main(string[] args)
+    // {
+    //     int t = Convert.ToInt32(Console.ReadLine().Trim());
 
-        for (int tItr = 0; tItr < t; tItr++)
-        {
-            int n = Convert.ToInt32(Console.ReadLine().Trim());
+    //     for (int tItr = 0; tItr < t; tItr++)
+    //     {
+    //         int n = Convert.ToInt32(Console.ReadLine().Trim());
 
-            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+    //         List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
 
-            List<int> result = MaximumSumSubArray.maxSumSubArray(arr);
+    //         List<int> result = MaximumSumSubArray.maxSumSubArray(arr);
 
-            Console.WriteLine(String.Join(" ", result));
-        }
+    //         Console.WriteLine(String.Join(" ", result));
+    //     }
 
-        Console.Read();
-    }
+    //     Console.Read();
+    // }
 }
