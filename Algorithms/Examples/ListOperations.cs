2fc008c26ae981355cbc387dd99862a8f6991015
abhinav medikonda <abhinavmedikonda@hotmail.com>
2024-06-21 23:43:27 -0700

    * minHeap

---
+++ Algorithms/Examples/ListOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Examples;
+
+public class ListOperations
+{
+    private static void Method(){
+        var list = new List<int>{8, 5, 9};
+        list.Remove(8); //remove element
+        var index = list.FindIndex(x => x == 8);
+        if(index >= 0) list.RemoveAt(index); //remove element at index
+
+        Console.Read();
+    }
+
+    // public static void Main(string[] args){
+    //     Method();
+    // }
+}
--- Algorithms/heap/MinHeap.cs
+++ Algorithms/heap/MinHeap.cs
@@ -6,98 +6,55 @@ namespace Algorithms.Heap
 {
     public class MinHeap
     {
-        /// <summary>
-        /// menu:
-        /// 1. add new number to heap
-        /// 2. delete number from heap
-        /// 3. print the minimum number in the heap
-        /// </summary>
-        public static int? minHeap(List<int> heap, List<int> arr)
-        {
-            if (arr[0] == 1) add(heap, arr[1]);
-            else if (arr[0] == 2) delete(heap, arr[1]);
-            else if (arr.Count > 0) return heap[0];
+        static void MinHeapifyUp(List<int> theHeap, int index){
+            if(index == 0){
+                return;
+            }
 
-            return null;
-        }
-
-        public static void add(List<int> heap, int num)
-        {
-            heap.Add(num);
-
-            heapify(heap, heap.Count - 1);
-        }
-
-        public static void heapify(List<int> heap, int ind)
-        {
-            while (ind > 0)
-            {
-                if (heap[ind] < heap[(ind - 1) / 2]) swap(heap, ind, (ind - 1) / 2);
-
-                ind = (ind - 1) / 2;
+            var parentIndex = (index-1)/2;
+            
+            if(theHeap[index] < theHeap[parentIndex]){
+                SwapItemsWithIndex(theHeap, index, parentIndex);
+                MinHeapifyUp(theHeap, parentIndex);
             }
         }
-
-        public static void delete(List<int> heap, int num)
-        {
-            var nullableInd = find(heap, num, 0);
-
-            if (nullableInd == null) return;
-
-            var ind = nullableInd ?? 0;
-
-            swap(heap, ind, heap.Count - 1);
-            heap.RemoveAt(heap.Count - 1);
-
-            if (ind == heap.Count) return;
-
-            if (heap[(ind - 1) / 2] > heap[ind])
-            {
-                heapify(heap, ind);
+        
+        static void MinHeapifyDown(List<int> theHeap, int index){
+            if((index*2)+1 > theHeap.Count-1){
+                return;
             }
-            else
-            {
-                while (true)
-                {
-                    if (2 * ind + 2 < heap.Count && heap[2 * ind + 2] < heap[2 * ind + 1])
-                    {
-                        swap(heap, ind, 2 * ind + 2);
-                        ind = 2 * ind + 2;
-                    }
-                    else if (2 * ind + 1 < heap.Count)
-                    {
-                        swap(heap, ind, 2 * ind + 1);
-                        ind = 2 * ind + 1;
-                    }
 
-                    break;
-                }
+            int childIndex = default;
+            if(theHeap.Count-1 > (index*2)+1 && theHeap[(index*2)+2] < theHeap[(index*2)+1]){
+                childIndex = (index*2)+2;
+            }
+            else{
+                childIndex = (index*2)+1;
+            }
+            
+            if(theHeap[childIndex] < theHeap[index]){
+                SwapItemsWithIndex(theHeap, index, childIndex);
+                MinHeapifyDown(theHeap, childIndex);
             }
         }
-
-        public static int? find(List<int> heap, int num, int ind)
-        {
-            if (ind >= heap.Count || num < heap[ind]) return null;
-
-            if (heap[ind] == num) return ind;
-
-            var res = find(heap, num, 2 * ind + 1);
-            if (res != null) return res;
-
-            return find(heap, num, 2 * ind + 2);
-        }
-
-        public static void swap(List<int> heap, int ind1, int ind2)
-        {
-            var temp = heap[ind2];
-            heap[ind2] = heap[ind1];
-            heap[ind1] = temp;
+        
+        static void SwapItemsWithIndex(List<int> theHeap, int a, int b){
+            var temp = theHeap[a];
+            theHeap[a] = theHeap[b];
+            theHeap[b] = temp;
         }
 
 /*
 12
 1 3
 1 65
+1 10
+1 4
+1 44
+1 67
+1 99
+1 24
+1 9
 2 65
 3
 2 3
@@ -110,21 +67,43 @@ namespace Algorithms.Heap
 2 7
 */
 
-        // static void Main(String[] args)
-        // {
-        //    var heap = new List<int>();
-        //    int t = Convert.ToInt32(Console.ReadLine().Trim());
-
-        //    for (int tItr = 0; tItr < t; tItr++)
-        //    {
-        //        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(heapTemp => Convert.ToInt32(heapTemp)).ToList();
-
-        //        var result = MinHeap.minHeap(heap, arr);
-
-        //        if (result != null) Console.WriteLine(result);
-        //    }
-
-        //    Console.Read();
-        // }
+        /// <summary>
+        /// menu:
+        /// 1. add new number to heap
+        /// 2. delete number from heap
+        /// 3. print the minimum number in the heap
+        /// </summary>
+        static void Main(String[] args)
+        {
+            var heap = new List<int>();
+        
+            var operations = Convert.ToInt32(Console.ReadLine());
+            
+            for(int i=0; i<operations; i++){
+                var input = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+                switch(input[0]){
+                    case 1:
+                        heap.Add(input[1]);
+                        MinHeapifyUp(heap, heap.Count-1);
+                        break;
+                    case 2:
+                        var index = heap.FindIndex(x => x == input[1]);
+                        SwapItemsWithIndex(heap, index, heap.Count-1);
+                        heap.RemoveAt(heap.Count-1);
+                        if(index == heap.Count) continue;
+                        if(heap[index] < input[1]){
+                            MinHeapifyUp(heap, index);
+                        }
+                        else{
+                            MinHeapifyDown(heap, index);
+                        }
+            
+                        break;
+                    case 3:
+                        Console.WriteLine(heap[0]);
+                        break;
+                }
+            }
+        }
     }
 }
