e8e03b13d22b257b025c9847ff3f45a9f3f4fd70
abhinav medikonda <abhinavmedikonda@hotmail.com>
2024-07-15 23:08:45 -0700

    + MinSwapsToSort

---
+++ Algorithms/Dictionary/MinSwapsToSort.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Dictionary;
+
+/// <summary>
+/// Find the minimum swaps required to sort a given list(ascending or decending)
+/// </summary>
+class MinSwapsToSort
+{
+    public static int minSwapsToSort(List<int> arr)
+    {
+        var ascending = new List<int>(arr);
+        ascending.Sort();
+        var decending = new List<int>(ascending);
+        decending.Reverse();
+
+        var aCount = countSwaps(arr, ascending);
+        var dCount = countSwaps(arr, decending);
+
+        return Math.Min(aCount, dCount);
+    }
+
+    private static int countSwaps(List<int> arr, List<int> sorted){
+        var result = 0;
+        var hash = new Dictionary<int, int>();
+        for(int i=0; i<arr.Count; i++){
+            hash.Add(arr[i], i);
+        }
+
+        for(int i=0; i<arr.Count; i++){
+            if(arr[i] != sorted[i]){
+                var swapIndex = hash[sorted[i]];
+                var temp = arr[i];
+                arr[i] = arr[swapIndex];
+                arr[swapIndex] = temp;
+                temp = hash[arr[i]];
+                hash[arr[i]] = hash[arr[swapIndex]];
+                hash[arr[swapIndex]] = temp;
+                result++;
+            }
+        }
+        
+        return result;
+    }
+
+/*
+5
+3 4 2 5 1
+*/
+
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = MinSwapsToSort.minSwapsToSort(arr);
+
+        Console.WriteLine(result);
+
+        Console.Read();
+    }
+}
--- Algorithms/Dictionary/PhonePadConversion.cs
+++ Algorithms/Dictionary/PhonePadConversion.cs
@@ -2,47 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Algorithms.Dictionary
+namespace Algorithms.Dictionary;
+
+public class PhonePadConversion
 {
-    public class PhonePadConversion
+    private static readonly Dictionary<char, string> hash = new Dictionary<char, string>{
+    { '2', "abc" }, { '3', "def" }, { '4', "ghi" }, { '5', "jkl" },
+    { '6', "mno" }, { '7', "pqrs" }, { '8', "tuv" }, { '9', "wxyz" },
+    };
+
+
+    public static IList<string> LetterCombinations(string digits)
     {
-        private static readonly Dictionary<char, string> hash = new Dictionary<char, string>{
-        { '2', "abc" }, { '3', "def" }, { '4', "ghi" }, { '5', "jkl" },
-        { '6', "mno" }, { '7', "pqrs" }, { '8', "tuv" }, { '9', "wxyz" },
-        };
+        return LetterCombinationsRecurssive(string.Empty, digits);
+    }
 
+    private static IList<string> LetterCombinationsRecurssive(string letters, string digits)
+    {
+        if (string.IsNullOrEmpty(digits)) return new List<string> { letters };
 
-        public static IList<string> LetterCombinations(string digits)
+        var result = new List<string>();
+        foreach (var c in hash[digits[0]])
         {
-            return LetterCombinationsRecurssive(string.Empty, digits);
+            result.AddRange(LetterCombinationsRecurssive(letters + c, digits.Substring(1, digits.Length - 1)));
         }
 
-        private static IList<string> LetterCombinationsRecurssive(string letters, string digits)
-        {
-            if (string.IsNullOrEmpty(digits)) return new List<string> { letters };
-
-            var result = new List<string>();
-            foreach (var c in hash[digits[0]])
-            {
-                result.AddRange(LetterCombinationsRecurssive(letters + c, digits.Substring(1, digits.Length - 1)));
-            }
-
-            return result;
-        }
+        return result;
+    }
 
 /*
 645
 */
 
-        //public static void Main(string[] args)
-        //{
-        //    string str = Console.ReadLine().Trim();
+    //public static void Main(string[] args)
+    //{
+    //    string str = Console.ReadLine().Trim();
 
-        //    var response = PhonePadConversion.LetterCombinations(str);
+    //    var response = PhonePadConversion.LetterCombinations(str);
 
-        //    response.ToList().ForEach(x => Console.WriteLine(x));
+    //    response.ToList().ForEach(x => Console.WriteLine(x));
 
-        //    Console.Read();
-        //}
-    }
+    //    Console.Read();
+    //}
 }
