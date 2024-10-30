43fa7a8b9ed3f02063a99b4cb55e272096128b48
abhinav medikonda <abhinavmedikonda@hotmail.com>
2024-10-30 11:41:26 -0400

    * easy

--- Algorithms/Arrays/MaxStockProfit.cs
+++ Algorithms/Arrays/MaxStockProfit.cs
@@ -13,22 +13,14 @@ public class MaxStockProfit
     */
     public static List<int> maxStockProfit(List<int> arr)
     {
-        int lastMin = arr[0], last = arr[0], profit = 0;
+        int profit = 0;
 
         for (int i = 1; i < arr.Count; i++)
         {
-            if(arr[i] < arr[i-1])
-            {
-                profit += arr[i - 1] - lastMin;
-                lastMin = arr[i];
-            }
+            // profit = Math.Max(profit, profit+arr[i]-arr[i-1]);
 
-            last = arr[i];
-
-            if(i == arr.Count - 1)
-            {
-                profit += arr[i] - lastMin;
-            }
+            var change = arr[i]-arr[i-1];
+            profit = change > 0 ? profit+change : profit;
         }
 
         return new List<int> { profit };
@@ -42,8 +34,8 @@ public class MaxStockProfit
 10 8 6 5 4 2
 */
 
-    //public static void Main(string[] args)
-    //{
+    // public static void Main(string[] args)
+    // {
     //    int t = Convert.ToInt32(Console.ReadLine().Trim());
 
     //    for (int tItr = 0; tItr < t; tItr++)
@@ -58,5 +50,5 @@ public class MaxStockProfit
     //    }
 
     //    Console.Read();
-    //}
+    // }
 }
