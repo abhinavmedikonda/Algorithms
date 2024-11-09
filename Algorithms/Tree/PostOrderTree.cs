f949660368d0b325c49ed588c2cd24c1d7a8129a
abhinav medikonda <abhinavmedikonda@hotmail.com>
2024-11-08 22:15:35 -0500

    + recursion

--- Algorithms/Tree/PostOrderTree.cs
+++ Algorithms/Tree/PostOrderTree.cs
@@ -7,31 +7,50 @@ using System.Security.Cryptography.X509Certificates;
 namespace Algorithms.Tree;
 
 public class PostOrderTree {
-	public static BTree fromArray(List<int> post, List<char> isLeaf){
-		var returns = new BTree{value = post[post.Count-1]};
-		var node = returns;
-		var stack = new Stack<BTree>();
+	// public static BTree fromArray(List<int> post, List<char> isLeaf){
+	// 	var node = new BTree{value = post[post.Count-1]};
+	// 	var stack = new Stack<BTree>();
 
-		for(int i=post.Count-2; i>=0; i--){
-			if(node.right == null){
-				node.right = new BTree{ value = post[i] };
-				if(isLeaf[i] == 'n'){
-					stack.Push(node);
-					node = node.right;
-				}
-			}
-			else if(node.left == null){
-				node.left = new BTree{ value = post[i] };
-				if(isLeaf[i] == 'n'){
-					stack.Push(node);
-					node = node.left;
-				}
-			}
-			else{
-				while(node.left != null){
-					node = stack.Pop();
-				}
-			}
+	// 	for(int i=post.Count-2; i>=0; i--){
+	// 		if(node.right == null){
+	// 			node.right = new BTree{ value = post[i] };
+	// 			if(isLeaf[i] == 'n'){
+	// 				stack.Push(node);
+	// 				node = node.right;
+	// 			}
+	// 		}
+	// 		else if(node.left == null){
+	// 			node.left = new BTree{ value = post[i] };
+	// 			if(isLeaf[i] == 'n'){
+	// 				stack.Push(node);
+	// 				node = node.left;
+	// 			}
+	// 		}
+	// 		else{
+	// 			while(node.left != null){
+	// 				node = stack.Pop();
+	// 			}
+	// 		}
+	// 	}
+
+	// 	return stack.First();
+	// }
+
+	public static BTree fromArray(List<int> post, List<char> isLeaf){
+		var index = post.Count;
+		return fromArrayRecur(post, isLeaf, ref index);
+	}
+
+	private static BTree fromArrayRecur(List<int> post, List<char> isLeaf, ref int index){
+		index--;
+		if(index < 0){
+			return null;
+		}
+
+		var returns = new BTree{value = post[index]};
+		if(isLeaf[index] == 'n'){
+			returns.right = fromArrayRecur(post, isLeaf, ref index);
+			returns.left = fromArrayRecur(post, isLeaf, ref index);
 		}
 
 		return returns;
@@ -49,7 +68,5 @@ y y n n n
 	// 	var isLeaf = Console.ReadLine().Split(' ').ToList().Select(x => char.Parse(x)).ToList();
 		
 	// 	BTree.postOrder(fromArray(post, isLeaf));
-
-	// 	Console.Read();
 	// }
 }
--- Algorithms/Tree/PreOrderTree.cs
+++ Algorithms/Tree/PreOrderTree.cs
@@ -7,31 +7,50 @@ using System.Security.Cryptography.X509Certificates;
 namespace Algorithms.Tree;
 
 public class PreOrderTree {
-	public static BTree fromArray(List<int> pre, List<char> isLeaf){
-		var returns = new BTree{value = pre[0]};
-		var node = returns;
-		var stack = new Stack<BTree>();
+	// public static BTree fromArray(List<int> pre, List<char> isLeaf){
+	// 	var node = new BTree{value = pre[0]};
+	// 	var stack = new Stack<BTree>();
 
-		for(int i=1; i<pre.Count; i++){
-			if(node.left == null){
-				node.left = new BTree{value = pre[i]};
-				if(isLeaf[i] == 'n'){
-					stack.Push(node);
-					node = node.left;
-				}
-			}
-			else if(node.right == null){
-				node.right = new BTree{value = pre[i]};
-				if(isLeaf[i] == 'n'){
-					stack.Push(node);
-					node = node.right;
-				}
-			}
-			else{
-				while(node.right != null){
-					node = stack.Pop();
-				}
-			}
+	// 	for(int i=1; i<pre.Count; i++){
+	// 		if(node.left == null){
+	// 			node.left = new BTree{value = pre[i]};
+	// 			if(isLeaf[i] == 'n'){
+	// 				stack.Push(node);
+	// 				node = node.left;
+	// 			}
+	// 		}
+	// 		else if(node.right == null){
+	// 			node.right = new BTree{value = pre[i]};
+	// 			if(isLeaf[i] == 'n'){
+	// 				stack.Push(node);
+	// 				node = node.right;
+	// 			}
+	// 		}
+	// 		else{
+	// 			while(node.right != null){
+	// 				node = stack.Pop();
+	// 			}
+	// 		}
+	// 	}
+
+	// 	return stack.First();
+	// }
+
+	public static BTree fromArray(List<int> pre, List<char> isLeaf){
+		var index = -1;
+		return fromArrayRecur(pre, isLeaf, ref index);
+	}
+
+	private static BTree fromArrayRecur(List<int> pre, List<char> isLeaf, ref int index){
+		index++;
+		if(index >= pre.Count){
+			return null;
+		}
+
+		var returns = new BTree{value=pre[index]};
+		if(isLeaf[index] == 'n'){
+			returns.left = fromArrayRecur(pre, isLeaf, ref index);
+			returns.right = fromArrayRecur(pre, isLeaf, ref index);
 		}
 
 		return returns;
@@ -43,11 +62,11 @@ public class PreOrderTree {
 n y n y y
 */
 
-	public static void Main(string[] args){
-		var n = int.Parse(Console.ReadLine());
-		var pre = Console.ReadLine().Split(' ').ToList().Select(x => int.Parse(x)).ToList();
-		var isLeaf = Console.ReadLine().Split(' ').ToList().Select(x => char.Parse(x)).ToList();
+	// public static void Main(string[] args){
+	// 	var n = int.Parse(Console.ReadLine());
+	// 	var pre = Console.ReadLine().Split(' ').ToList().Select(x => int.Parse(x)).ToList();
+	// 	var isLeaf = Console.ReadLine().Split(' ').ToList().Select(x => char.Parse(x)).ToList();
 		
-		BTree.preOrder(fromArray(pre, isLeaf));
-	}
+	// 	BTree.preOrder(fromArray(pre, isLeaf));
+	// }
 }
