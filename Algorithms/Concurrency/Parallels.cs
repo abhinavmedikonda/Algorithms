b73b4cfad9bac8be5c5b67ddae7d7c1042aa4da0
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-01-09 14:53:07 -0500

    + Parallels

--- Algorithms/Concurrency/Parallels.cs
+++ Algorithms/Concurrency/Parallels.cs
@@ -11,33 +11,70 @@ public class Parallels
 {
 
 	private static void Start(){
-		var s = "hello, world";
+		ParallelOptions parallelOptions = new();
+		parallelOptions.MaxDegreeOfParallelism = 15;
+		Console.WriteLine($"MaxDegreeOfParallelism: {parallelOptions.MaxDegreeOfParallelism}");
 
-		foreach (var c in s)
-		{
-			if(c == ','){
-				break;
-			}
+		var rnd = new Random();
+        int breakIndex = rnd.Next(1, 11);
 
-			Console.Write(c);
-		}
+        Console.WriteLine($"Will call Break at iteration {breakIndex}\n");
+
+        var result = Parallel.For(1, 101, parallelOptions, (i, state) => 
+        {
+            Console.WriteLine($"Beginning iteration {i}");
+            int delay;
+            lock (rnd)
+                delay = rnd.Next(1, 1001);
+            Thread.Sleep(delay);
+
+            if (state.ShouldExitCurrentIteration)
+            {
+                if (state.LowestBreakIteration < i){
+		            Console.WriteLine($"returns in iteration {i}, delay: {delay}");
+                    return;
+				}
+            }
+
+            if (i == breakIndex)
+            {
+                Console.WriteLine($"Break in iteration {i}");
+                state.Break();
+            }
+
+            Console.WriteLine($"Completed iteration {i}, delay: {delay}");
+        });
+
+        if (result.LowestBreakIteration.HasValue)
+            Console.WriteLine($"\nLowest Break Iteration: {result.LowestBreakIteration}");
+        else
+            Console.WriteLine($"\nNo lowest break iteration.");
 
 		Console.WriteLine();
-		Parallel.For(0, s.Length, (i, loopState) => {
-			if(s[i] == ','){
+		Parallel.For(0, 15, parallelOptions, (i, loopState) => {
+			if(i == 3){
 				loopState.Break();
 			}
 
-			Console.Write(s[i]);
+			if(loopState.ShouldExitCurrentIteration){
+				Console.WriteLine("true");
+			}
+			if(!loopState.ShouldExitCurrentIteration || loopState.LowestBreakIteration < i){
+				SudokuSolver.printBoard(SudokuSolver.GetSolution(SudokuSolver.GetBoard()));
+				Console.WriteLine(i);
+			}
 		});
 
+		var s = "Executes a foreach (For Each in Visual Basic) operation in which iterations may run in parallel";
 		Console.WriteLine();
-		Parallel.ForEach(s, (c, loopState) => {
-			if(c == ','){
+		Parallel.ForEach(s, (c, loopState, l) => {
+			if(l == 3){
 				loopState.Stop();
 			}
 
-			Console.Write(c);
+			if(!loopState.IsStopped){
+				Console.Write(c);
+			}
 		});
 	}
 
